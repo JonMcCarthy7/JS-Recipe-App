bbcc207895cf6eeb8273ef51e565566da02c92e5
Jon McCarthy <jtmcc93@gmail.com>
2018-06-02 16:25:01 -0700

    three different ways to import/export ES6 modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
-// Global app controller
-import x from "./test";
-const y = 12;
-console.log(x);
-console.log(y);
+import str from "./models/Search";
+// import { add as a, mult as m } from "./views/searchView";
+import * as searchView from "./views/searchView";
+
+console.log(
+  `${str}. ${searchView.add(
+    searchView.ID,
+    3
+  )} is the add function. ${searchView.mult(5, 5)} is the multiply function.`
+);
