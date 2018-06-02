8b2bb998fdd08cb0d498cb772b7fa87446ac7531
Jon McCarthy <jtmcc93@gmail.com>
2018-06-02 16:56:59 -0700

    make API using async/await. add try/catch

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
-async function getResults(query) {}
+import axios from "axios";
+async function getResults(query) {
+  const proxy = "https:/cors-anywhere.herokuapp.com/";
+  const key = "cd53e8c06b1860ce99b42fd2f4db55ce";
+  try {
+    const res = await axios(
+      `${proxy}http://food2fork.com/api/search?key=${key}&?q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
 
-getResults();
-
-//http://food2fork.com/api/search
-//cd53e8c06b1860ce99b42fd2f4db55ce;
+getResults("pizza");
