22799ed7a5177c8c97655d9c2e0305a98e476e14
Jon McCarthy <jtmcc93@gmail.com>
2018-06-02 18:19:28 -0700

    create Search class, add getResults function to Search

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,4 @@
-import axios from "axios";
-async function getResults(query) {
-  const proxy = "https:/cors-anywhere.herokuapp.com/";
-  const key = "cd53e8c06b1860ce99b42fd2f4db55ce";
-  try {
-    const res = await axios(
-      `${proxy}http://food2fork.com/api/search?key=${key}&?q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
+import Search from "./models/Search";
 
-getResults("pizza");
+const search = new Search("pizza").getResults();
+console.log(search);
