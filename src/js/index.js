7679cb9fd609e85d42f5b33f30107023a8ee427a
Jon McCarthy <jtmcc93@gmail.com>
2018-06-04 22:55:26 -0700

    add update servings ingredients

--- src/js/index.js
+++ src/js/index.js
@@ -91,7 +91,7 @@ const controlRecipe = async () => {
       state.recipe.calcTime();
       state.recipe.calcServings();
       // Render recipe
-      // console.log(state.recipe);
+      console.log(state.recipe);
       clearLoader();
       recipeView.renderRecipe(state.recipe);
     } catch (error) {
@@ -106,3 +106,19 @@ const controlRecipe = async () => {
 ["hashchange", "load"].forEach(event =>
   window.addEventListener(event, controlRecipe)
 );
+
+// Handling recipe button clicks
+elements.recipe.addEventListener("click", e => {
+  if (e.target.matches(".btn-decrease, .btn-decrease *")) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings("dec");
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches(".btn-increase, .btn-increase *")) {
+    // Increase button is clicked
+    state.recipe.updateServings("inc");
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+  console.log(state.recipe);
+});
